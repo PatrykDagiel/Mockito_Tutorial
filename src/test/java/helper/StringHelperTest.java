8f45c9a1da8e0baece77fae30248b96f163bfe9d
dagiel <patryk.dagiel@gmail.com>
2018-04-04 09:24:14 +0200

    JUnit - step 3 done

--- src/test/java/helper/StringHelperTest.java
+++ src/test/java/helper/StringHelperTest.java
@@ -2,11 +2,18 @@ package helper;
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.fail;
+
 public class StringHelperTest {
 
     @Test
     public void truncateAInFirst2Positions() {
-        }
+        StringHelper helper = new StringHelper();
+        assertEquals("CD", helper.truncateAInFirst2Positions("AACD"));
+        assertEquals("CD", helper.truncateAInFirst2Positions("ACD"));
+    }
+
 
     @Test
     public void areFirstAndLastTwoCharactersTheSame(){
