b8d0dab618b7e73d50942a29fdfe7af5d2d8f1da
Dagiel, Patryk (Nokia - PL/Wroclaw) <patryk.dagiel@nokia.com>
2018-06-06 09:47:44 +0200

    Video 11/36 done, p.2

--- src/test/java/helper/StringParametrizedTest.java
+++ src/test/java/helper/StringParametrizedTest.java
@@ -12,6 +12,8 @@ import static junit.framework.TestCase.assertTrue;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 
+// Parametrized testing
+
 @RunWith(Parameterized.class)
 public class StringParametrizedTest {
 
